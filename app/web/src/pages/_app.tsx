c93ad393b8c58e9347d85f2e46c9b06ce837ad0c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-01-18 09:36:36 +0900

    Create _app.tsx

---
+++ app/web/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+import "pages/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
